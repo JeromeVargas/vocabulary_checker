2e9ce8dfb8f84ef2465d6654e09db0de2dc622f9
Jerome Vargas <jero124@yahoo.ca>
2024-02-06 17:09:09 -0500

    refactor: get the images state from a json

--- src/hooks/useImages.ts
+++ src/hooks/useImages.ts
@@ -1,25 +1,10 @@
 import { useMemo, useState } from "react";
+
+import data from "../data/images.json";
 import randomNumber from "../lib/utils/random";
 
 export const useImages = () => {
-  const [images, setImages] = useState([
-    {
-      url: "https://sb-cafetalk.s3.amazonaws.com/user-uploaded-files/cafetalk-optimized-618d23a685de3-954832700-1636639654.jpg",
-      word: "hanashimashou",
-    },
-    {
-      url: "https://www.universityofcalifornia.edu/sites/default/files/styles/article_default_banner/public/improve-talk-uc-san-diego.jpg?h=792bbb88&itok=rH_V951k",
-      word: "kaiwa",
-    },
-    {
-      url: "https://engfluent.com/wp-content/uploads/2016/09/english-listening-practice.png",
-      word: "kiku_renshou",
-    },
-    {
-      url: "https://www.callcentrehelper.com/images/stories/2022/08/vocabulary-293796887-760.jpg",
-      word: "tango_hyou",
-    },
-  ]);
+  const [images, setImages] = useState(data);
   const [input, setInput] = useState("");
   const [result, setResult] = useState("");
   const [isInputReady, setIsInputReady] = useState(false);
@@ -53,25 +38,7 @@ export const useImages = () => {
   };
 
   const handleReset = () => {
-    setImages((prevArray) => [
-      ...prevArray,
-      {
-        url: "https://sb-cafetalk.s3.amazonaws.com/user-uploaded-files/cafetalk-optimized-618d23a685de3-954832700-1636639654.jpg",
-        word: "hanashimashou",
-      },
-      {
-        url: "https://www.universityofcalifornia.edu/sites/default/files/styles/article_default_banner/public/improve-talk-uc-san-diego.jpg?h=792bbb88&itok=rH_V951k",
-        word: "kaiwa",
-      },
-      {
-        url: "https://engfluent.com/wp-content/uploads/2016/09/english-listening-practice.png",
-        word: "kiku_renshou",
-      },
-      {
-        url: "https://www.callcentrehelper.com/images/stories/2022/08/vocabulary-293796887-760.jpg",
-        word: "tango_hyou",
-      },
-    ]);
+    setImages(data);
   };
   return {
     handleCheck,
