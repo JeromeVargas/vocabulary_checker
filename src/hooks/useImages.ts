4ee922179116a58a1a0b43b6cc92568a8e1edbf7
Jerome Vargas <jero124@yahoo.ca>
2024-02-06 15:52:55 -0500

    refactor: use external urls

--- src/hooks/useImages.ts
+++ src/hooks/useImages.ts
@@ -3,10 +3,22 @@ import randomNumber from "../lib/utils/random";
 
 export const useImages = () => {
   const [images, setImages] = useState([
-    { url: "src/assets/hanashimashou.png", word: "hanashimashou" },
-    { url: "src/assets/kaiwa.png", word: "kaiwa" },
-    { url: "src/assets/kiku_renshou.png", word: "kiku_renshou" },
-    { url: "src/assets/tango_hyou.png", word: "tango_hyou" },
+    {
+      url: "https://sb-cafetalk.s3.amazonaws.com/user-uploaded-files/cafetalk-optimized-618d23a685de3-954832700-1636639654.jpg",
+      word: "hanashimashou",
+    },
+    {
+      url: "https://www.universityofcalifornia.edu/sites/default/files/styles/article_default_banner/public/improve-talk-uc-san-diego.jpg?h=792bbb88&itok=rH_V951k",
+      word: "kaiwa",
+    },
+    {
+      url: "https://engfluent.com/wp-content/uploads/2016/09/english-listening-practice.png",
+      word: "kiku_renshou",
+    },
+    {
+      url: "https://www.callcentrehelper.com/images/stories/2022/08/vocabulary-293796887-760.jpg",
+      word: "tango_hyou",
+    },
   ]);
   const [input, setInput] = useState("");
   const [result, setResult] = useState("");
@@ -43,10 +55,22 @@ export const useImages = () => {
   const handleReset = () => {
     setImages((prevArray) => [
       ...prevArray,
-      { url: "src/assets/hanashimashou.png", word: "hanashimashou" },
-      { url: "src/assets/kaiwa.png", word: "kaiwa" },
-      { url: "src/assets/kiku_renshou.png", word: "kiku_renshou" },
-      { url: "src/assets/tango_hyou.png", word: "tango_hyou" },
+      {
+        url: "https://sb-cafetalk.s3.amazonaws.com/user-uploaded-files/cafetalk-optimized-618d23a685de3-954832700-1636639654.jpg",
+        word: "hanashimashou",
+      },
+      {
+        url: "https://www.universityofcalifornia.edu/sites/default/files/styles/article_default_banner/public/improve-talk-uc-san-diego.jpg?h=792bbb88&itok=rH_V951k",
+        word: "kaiwa",
+      },
+      {
+        url: "https://engfluent.com/wp-content/uploads/2016/09/english-listening-practice.png",
+        word: "kiku_renshou",
+      },
+      {
+        url: "https://www.callcentrehelper.com/images/stories/2022/08/vocabulary-293796887-760.jpg",
+        word: "tango_hyou",
+      },
     ]);
   };
   return {
