9c579767cc587caa803dc79622ea0a143f490076
Jerome Vargas <jero124@yahoo.ca>
2024-07-05 10:38:29 -0500

    styles: label desing system colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,18 +5,22 @@ export default {
   theme: {
     extend: {
       colors: {
+        // blue
         base: {
           main: "#60A5FA",
           shade: "#93C5FD",
         },
+        // white
         font: {
           main: "#FFFFFF",
         },
+        // green
         neutral: {
           main: "#4ADE80",
           shade: "#86EFAC",
           light: "#BBF7D0",
         },
+        // orange / red
         accent: {
           main: "#FB923C",
           shade: "#FDBA74",
