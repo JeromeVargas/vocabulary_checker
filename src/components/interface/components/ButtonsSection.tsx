8cd509f383f7da76e5fc68932633993ff595c4d9
Jerome Vargas <jero124@yahoo.ca>
2024-07-06 09:52:46 -0500

    refactor: turn link from button to actual link

--- src/components/interface/components/ButtonsSection.tsx
+++ src/components/interface/components/ButtonsSection.tsx
@@ -76,9 +76,12 @@ const ButtonsSection = ({
           >
             Try Again
           </button>
-          <button className="w-10/12 rounded-xl border-4 bg-neutral-shade px-10 py-4 text-center font-normal text-font-main opacity-90 hover:-translate-y-1 hover:translate-x-1 hover:bg-accent-main md:w-auto">
-            <Link to="/">New Exercise</Link>
-          </button>
+          <Link
+            className="w-10/12 rounded-xl border-4 bg-neutral-shade px-10 py-4 text-center font-normal text-font-main opacity-90 hover:-translate-y-1 hover:translate-x-1 hover:bg-accent-main md:w-auto"
+            to="/"
+          >
+            New Exercise
+          </Link>
         </>
       )}
     </>
