fe4c4a4f4b136b635615aa06cf24e46132a42268
Jerome Vargas <jero124@yahoo.ca>
2024-07-06 10:11:56 -0500

    refactor: create button handler components

--- src/components/Error404Page.tsx
+++ src/components/Error404Page.tsx
@@ -2,10 +2,13 @@ import { Link } from "react-router-dom";
 
 export default function ErrorPage() {
   return (
-    <main className="bg-base-main text-font-main flex h-screen w-full flex-col items-center justify-center text-center text-5xl font-black">
-      <h1 className="hover:bg-accent-main w-10/12 rounded-xl border-4 bg-neutral-shade py-4 text-center font-bold text-neutral-contrast">
-        <Link to="/">Please go back to the home page</Link>
-      </h1>
+    <main className="flex h-screen w-full flex-col items-center justify-center bg-base-main text-center text-5xl font-black text-font-main">
+      <Link
+        className="text-neutral-contrast w-10/12 rounded-xl border-4 bg-neutral-shade py-4 text-center font-bold hover:bg-accent-main"
+        to="/"
+      >
+        Please go back to the home page
+      </Link>
     </main>
   );
 }
--- src/components/interface/components/ButtonsSection.tsx
+++ src/components/interface/components/ButtonsSection.tsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
-import { cn } from "../../../lib/utils/classMerge";
 import { type image } from "../../../types";
+import ButtonHandleSpeech from "../../ButtonHandleSpeech";
+import ButtonHandleNext from "../../ButtonHandleNext";
+import ButtonHandleReset from "./ButtonHandleReset";
 
 type ButtonsSectionProps = {
   images: image[];
@@ -21,61 +23,12 @@ const ButtonsSection = ({
     <>
       {images.length > 0 ? (
         <>
-          <button
-            className="rounded-full border-2 border-accent-shade bg-base-shade transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-100 hover:border-accent-contrast"
-            type="button"
-            onClick={handleSpeech}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="5.5rem"
-              height="5.5rem"
-              viewBox="0 0 48 48"
-            >
-              <path
-                fill="#81d4fa"
-                d="M28 7.1v2c7.3 1 13 7.3 13 14.9s-5.7 13.9-13 14.9v2c8.4-1 15-8.2 15-16.9S36.4 8.1 28 7.1"
-              ></path>
-              <path
-                fill="#546e7a"
-                d="M14 32H7c-1.1 0-2-.9-2-2V18c0-1.1.9-2 2-2h7z"
-              ></path>
-              <path fill="#78909c" d="M26 42L14 32V16L26 6z"></path>
-              <path
-                fill="#03a9f4"
-                d="M28 17.3v2.1c1.8.8 3 2.5 3 4.6s-1.2 3.8-3 4.6v2.1c2.9-.9 5-3.5 5-6.7s-2.1-5.8-5-6.7"
-              ></path>
-              <path
-                fill="#4fc3f7"
-                d="M28 12.2v2c4.6.9 8 5 8 9.8s-3.4 8.9-8 9.8v2c5.7-1 10-5.9 10-11.8s-4.3-10.9-10-11.8"
-              ></path>
-            </svg>
-          </button>
-          <button
-            onClick={handleNext}
-            className={cn(
-              "w-60 select-none rounded-2xl border-4 px-10 py-4 text-center font-bold text-font-main",
-              {
-                "cursor-none bg-base-main": !speechReady,
-                "border-accent-shade bg-neutral-shade hover:-translate-y-1 hover:translate-x-1":
-                  speechReady,
-              },
-            )}
-            type="button"
-            disabled={!speechReady}
-          >
-            {!speechReady ? "Listen" : "Next"}
-          </button>
+          <ButtonHandleSpeech handleSpeech={handleSpeech} />
+          <ButtonHandleNext speechReady={speechReady} handleNext={handleNext} />
         </>
       ) : (
         <>
-          <button
-            className="w-10/12 rounded-xl border-4 bg-neutral-shade px-10 py-4 text-center font-normal text-font-main opacity-90 hover:-translate-y-1 hover:translate-x-1 hover:bg-accent-main md:w-auto"
-            onClick={handleReset}
-            type="button"
-          >
-            Try Again
-          </button>
+          <ButtonHandleReset handleReset={handleReset} />
           <Link
             className="w-10/12 rounded-xl border-4 bg-neutral-shade px-10 py-4 text-center font-normal text-font-main opacity-90 hover:-translate-y-1 hover:translate-x-1 hover:bg-accent-main md:w-auto"
             to="/"
