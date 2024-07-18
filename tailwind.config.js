98192ac0a3beb3fa890bdd1ae76db5c2b5844565
Jerome Vargas <jero124@yahoo.ca>
2024-07-18 17:20:04 -0500

    chore: update tailwind color labeling

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,19 +7,19 @@ export default {
       colors: {
         base: {
           main: "rgb(var(--color-base-main))",
-          shade: "rgb(var(--color-base-shade))",
+          shade: "rgb(var(--color-base-shade))", // blue - black
         },
         font: {
           main: "rgb(var(--color-font-main))", // white
         },
         neutral: {
-          main: "rgb(var(--color-neutral-main))", // green
+          main: "rgb(var(--color-neutral-main))",
           shade: "rgb(var(--color-neutral-shade))",
-          light: "rgb(var(--color-neutral-light))",
+          light: "rgb(var(--color-neutral-light))", // green - gray
         },
 
         accent: {
-          main: "rgb(var(--color-accent-main))", // orange
+          main: "rgb(var(--color-accent-main))", // orange - green
           shade: "rgb(var(--color-accent-shade))",
           contrast: "rgb(var(--color-accent-contrast))", // red
         },
