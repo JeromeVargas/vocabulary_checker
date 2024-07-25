dbcc2a5a7a95523fc08ffd4791c7d1c759f9ba41
Jerome Vargas <jero124@yahoo.ca>
2024-07-25 17:38:30 -0500

    test: add playwright end to end testing

---
+++ tests/home.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from "@playwright/test";
+
+const LOCAL_HOST_URL = "http://localhost:5173/";
+
+test.describe("home page", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(LOCAL_HOST_URL);
+  });
+
+  test("should have heading text", async ({ page }) => {
+    expect(await page.getByRole("heading").textContent()).toBe(
+      "シャドーイング アプリ",
+    );
+  });
+  test("link should redirect to interface", async ({ page }) => {
+    await page.getByRole("link", { name: "しょるい" }).click();
+
+    await expect(page.getByRole("button", { name: "Listen" })).toBeVisible();
+  });
+  test("button should turn dark mode", async ({ page }) => {
+    await page.getByRole("button").click();
+    await expect(page.locator("html")).toHaveAttribute("data-theme", "dark");
+
+    await page.getByRole("button").click();
+    await expect(page.locator("html")).toHaveAttribute("data-theme", "light");
+  });
+});
---
+++ tests/interface.spec.ts
@@ -0,0 +1,39 @@
+import { test, expect } from "@playwright/test";
+
+const LOCAL_HOST_URL_INTERFACE = "http://localhost:5173/instructions";
+
+test.describe("interface", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(LOCAL_HOST_URL_INTERFACE);
+  });
+
+  test("button should have text", async ({ page }) => {
+    await expect(page.getByRole("button", { name: "Listen" })).toBeVisible();
+  });
+  test("button should change the text", async ({ page }) => {
+    await page.getByRole("button").first().click();
+
+    await expect(page.getByRole("button", { name: "Next" })).toBeVisible();
+  });
+  test("exit screen should show new exercise link", async ({ page }) => {
+    await page.getByRole("button").first().click();
+    await page.getByRole("button", { name: "Next" }).click();
+
+    await page.getByRole("button").first().click();
+    await page.getByRole("button", { name: "Next" }).click();
+
+    await page.getByRole("button").first().click();
+    await page.getByRole("button", { name: "Next" }).click();
+
+    await page.getByRole("button").first().click();
+    await page.getByRole("button", { name: "Next" }).click();
+
+    await expect(
+      page.getByRole("link", { name: "New Exercise" }),
+    ).toBeVisible();
+    await page.getByRole("link", { name: "New Exercise" }).click();
+    expect(await page.getByRole("heading").textContent()).toBe(
+      "シャドーイング アプリ",
+    );
+  });
+});
