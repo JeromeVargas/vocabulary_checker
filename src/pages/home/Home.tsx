3c0cd8c52f043415a53fd64099a0e07f7caf38bb
Jerome Vargas <jero124@yahoo.ca>
2024-07-28 13:03:16 -0500

    styles: add header in home component

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -13,26 +13,30 @@ const Home = ({ theme, handleChangeTheme }: TopicsIndexProps) => {
   const { topics } = dataFetcher();
 
   return (
-    <main className="flex min-h-screen flex-col items-center bg-base-main p-2 text-center text-5xl font-black text-font-main">
-      <ThemeButton theme={theme} handleChangeTheme={handleChangeTheme} />
-      <h1 className="mt-10">シャドーイング アプリ</h1>
-      <section className="m-20">
-        <ul className="space-y-16">
-          {topics.map(({ topic, linkText }) => {
-            return (
-              <li key={topic}>
-                <Link
-                  className="rounded-xl border-4 bg-accent-main px-8 py-2 text-center text-2xl font-normal text-font-main opacity-90 hover:-translate-y-1 hover:translate-x-1 hover:bg-neutral-shade md:text-5xl"
-                  to={`/${topic}`}
-                >
-                  {linkText}
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </section>
-    </main>
+    <>
+      <header className="mt-16">
+        <ThemeButton theme={theme} handleChangeTheme={handleChangeTheme} />
+        <h1 className="text-font-main">シャドーイング アプリ</h1>
+      </header>
+      <main>
+        <section className="m-10 md:m-20">
+          <ul className="space-y-8 md:space-y-16">
+            {topics.map(({ topic, linkText }) => {
+              return (
+                <li key={topic}>
+                  <Link
+                    className="rounded-xl border-4 bg-accent-main px-8 py-2 text-center text-2xl font-normal text-font-main opacity-90 hover:-translate-y-1 hover:translate-x-1 hover:bg-neutral-shade md:text-5xl"
+                    to={`/${topic}`}
+                  >
+                    {linkText}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </section>
+      </main>
+    </>
   );
 };
 
