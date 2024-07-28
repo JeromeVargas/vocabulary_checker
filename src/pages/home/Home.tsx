610c917e67dd3ff43f1405d0205f4670b3f11ea4
Jerome Vargas <jero124@yahoo.ca>
2024-07-28 12:25:55 -0500

    styles: make home links into an actually list

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -16,18 +16,21 @@ const Home = ({ theme, handleChangeTheme }: TopicsIndexProps) => {
     <main className="flex min-h-screen flex-col items-center bg-base-main p-2 text-center text-5xl font-black text-font-main">
       <ThemeButton theme={theme} handleChangeTheme={handleChangeTheme} />
       <h1 className="mt-10">シャドーイング アプリ</h1>
-      <section className="m-12 flex flex-col gap-6">
-        {topics.map(({ topic, linkText }) => {
-          return (
-            <Link
-              key={topic}
-              className="rounded-xl border-4 bg-accent-main px-10 py-4 text-center text-2xl font-normal text-font-main opacity-90 hover:-translate-y-1 hover:translate-x-1 hover:bg-neutral-shade md:text-5xl"
-              to={`/${topic}`}
-            >
-              {linkText}
-            </Link>
-          );
-        })}
+      <section className="m-20">
+        <ul className="space-y-16">
+          {topics.map(({ topic, linkText }) => {
+            return (
+              <li key={topic}>
+                <Link
+                  className="rounded-xl border-4 bg-accent-main px-8 py-2 text-center text-2xl font-normal text-font-main opacity-90 hover:-translate-y-1 hover:translate-x-1 hover:bg-neutral-shade md:text-5xl"
+                  to={`/${topic}`}
+                >
+                  {linkText}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
       </section>
     </main>
   );
