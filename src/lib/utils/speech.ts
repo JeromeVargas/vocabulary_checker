1bae781e4c53b1648c9f709578b4943d2b96f232
Jerome Vargas <jero124@yahoo.ca>
2024-08-25 15:31:50 -0500

    refactor: move the utterance speech to the constants file

--- src/lib/utils/speech.ts
+++ src/lib/utils/speech.ts
@@ -1,10 +1,10 @@
-import { TARGET_LANGUAGE } from "../../config/constants";
+import { TARGET_LANGUAGE, UTTERANCE_SPEED } from "../../config/constants";
 
 const speechUtterance = (text: string) => {
   const speech = new SpeechSynthesisUtterance();
   speech.text = text;
   speech.lang = TARGET_LANGUAGE;
-  speech.rate = 0.7;
+  speech.rate = UTTERANCE_SPEED;
   window.speechSynthesis.speak(speech);
 };
 
