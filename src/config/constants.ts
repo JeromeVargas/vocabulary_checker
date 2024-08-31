e1062f91e3b23e8e96544a1b99341a84e5e74ebd
Jerome Vargas <jero124@yahoo.ca>
2024-08-31 10:22:33 -0500

    fix: add _redirect to prevent routing breaks

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,18 +1,18 @@
 /* Speech */
-export const TARGET_LANGUAGE: string = "en-US";
-export const UTTERANCE_SPEED: number = 0.7;
+export const TARGET_LANGUAGE: string = "ja-JP";
+export const UTTERANCE_SPEED: number = 0.9;
 
 /* Sections dynamic language strings  */
-export const HOME_HEADING: string = "Vocabulary App";
-export const ACTION_BUTTON_LISTEN: string = "Listen";
-export const ACTION_BUTTON_NEXT: string = "Next";
-export const CONGRATS_MESSAGE: string = "Congrats!";
-export const TRY_AGAIN_BUTTON: string = "Try Again";
-export const NEW_EXERCISE_LINK: string = "New Exercise";
-export const ERROR_MESSAGE_LINK: string = "Please go back to the home page";
+export const HOME_HEADING: string = "ぼきゃぶらりーあぷり";
+export const ACTION_BUTTON_LISTEN: string = "きく";
+export const ACTION_BUTTON_NEXT: string = "つぎへ";
+export const CONGRATS_MESSAGE: string = "おめでとう!";
+export const TRY_AGAIN_BUTTON: string = "もういちど";
+export const NEW_EXERCISE_LINK: string = "あたらしいれんしゅう";
+export const ERROR_MESSAGE_LINK: string = "ほーむぺーじにもどってください";
 
 /* Testing */
 // add this manually from the first meta-data JSON key
-export const TOPIC: string = "articles";
+export const TOPIC: string = "base-instructions";
 // add this manually from the previously mentioned key metadata array length
-export const TOPIC_NUMBER_OF_WORDS: number = 10;
+export const TOPIC_NUMBER_OF_WORDS: number = 4;
