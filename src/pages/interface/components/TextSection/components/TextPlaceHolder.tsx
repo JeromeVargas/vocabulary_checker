f094d9b15979147e75cd9cb9cc9f45ae43e7fd42
unknown <jero124@yahoo.ca>
2026-05-12 18:31:57 -0500

    fix: improve text plaholder border

--- src/pages/interface/components/TextSection/components/TextPlaceHolder.tsx
+++ src/pages/interface/components/TextSection/components/TextPlaceHolder.tsx
@@ -1,6 +1,6 @@
 const TextPlaceHolder = () => {
   return (
-    <span className="flex size-16 cursor-pointer items-center justify-center rounded-full bg-base-shade text-2xl font-semibold text-neutral-main ring-1 ring-neutral-shade/60 transition-colors duration-150 hover:ring-accent-main/80">
+    <span className="flex size-16 cursor-pointer items-center justify-center rounded-full bg-base-shade text-2xl font-semibold text-neutral-main ring-2 ring-neutral-main/60 transition-colors duration-150 hover:ring-accent-main">
       ?
     </span>
   );
